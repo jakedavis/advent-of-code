c591c7129c6abc96488b63448319330e7b141aaf
Jake Davis <jake.davis5989@gmail.com>
2020-12-08 11:31:09 -0800

    Working, more efficient solution

--- 2020/1_report_repair.rb
+++ 2020/1_report_repair.rb
@@ -7,21 +7,28 @@ class ReportRepair
 
   def initialize(vals)
     @vals  = vals
-    @found = nil, nil
+    @found = nil
   end
 
+  # The main trick here is slicing out the first entry in the array with i+1, which means we don't
+  # check extraneous values later on (in otherwords, the number of checks will decrease each
+  # iteration, versus checking the first value in the array which has already been verified).
   def find_values
-    @vals.each do |v|
-      @found ||= find_2020(arr)
+    @vals.each_with_index do |v, i|
+      @found ||= find_2020(v, @vals[i+1..])
+      break if found
     end
   end
 
-  def find_2020(arr)
-    first = arr.first
-
-    arr.drop(1).each do |v|
-      return first, v if first+v == 2020
+  # Pretty simple - return the product of two numbers whose sum is 2020. Return nil at the end is
+  # required because Array#each will return the array itself, and we're using ||= above. Probably a
+  # fancier way to do this.
+  def find_2020(first, arr)
+    arr.each do |v|
+      return first*v if first+v == 2020
     end
+
+    return nil
   end
 end
 
