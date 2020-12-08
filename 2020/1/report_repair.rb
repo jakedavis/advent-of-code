3fab73b100aa583900fd9bc6805aa003f884030b
Jake Davis <jake.davis5989@gmail.com>
2020-12-08 14:21:02 -0800

    Rewrite and extend for n > 2
    Before, this code assumed you were summing 2 digits. As opposed to simply
    adding a 3-digit method, this rewrites the logic to accept any n and derive
    the correct value from there. It does this using three queues, with a
    base queue essentially getting processed and thrown into one of the two
    others.
    
    I originally was trying to do this with Array#each, but I couldn't find
    a nice way to avoid processing the candidate numbers again. That's why I
    moved to the queue system.

--- 2020/1/report_repair.rb
+++ 2020/1/report_repair.rb
@@ -3,36 +3,52 @@
 # Day one of the AoC; report repair exercise
 
 class ReportRepair
-  attr_accessor :found
-
   def initialize(vals)
     @vals  = vals
-    @found = nil
   end
 
-  # The main trick here is slicing out the first entry in the array with i+1, which means we don't
-  # check extraneous values later on (in otherwords, the number of checks will decrease each
-  # iteration, versus checking the first value in the array which has already been verified).
-  def find_values
+  # This method is mostly the same as before, but accepts n digits you're looking to sum. It
+  # delegates most of the work to find_candidates.
+  def find_values(n=2)
     @vals.each_with_index do |v, i|
-      @found ||= find_2020(v, @vals[i+1..])
-      break if found
+      candidates = find_candidates([v], @vals[i+1..], n)
+
+      if candidates.length == n && candidates.reduce(:+) == 2020
+        return candidates.reduce(:*)
+      end
     end
+
+    return "None found"
   end
 
-  # Pretty simple - return the product of two numbers whose sum is 2020. Return nil at the end is
-  # required because Array#each will return the array itself, and we're using ||= above. Probably a
-  # fancier way to do this.
-  def find_2020(v, arr)
-    arr.each do |u|
-      return v*u if v+u == 2020
+  # To make this more generic, this method now accepts n digits to use for the summation. This
+  # should make it extensible for any n. This method manages three queues. Numbers are essentially
+  # moving from the pending array to EITHER processed or candidates depending on if they add up to
+  # less than or equal to 2020. If the length of the candidates array is less than we expect, we use
+  # recursion to find the next digit. Otherwise, we check if it's exactly 2020.
+  def find_candidates(candidates, pending, n)
+    processed = []
+
+    while pending.length > 0
+      candidates.push(pending.pop)
+    
+      if candidates.length < n 
+        if candidates.reduce(:+) <= 2020
+          find_candidates(candidates, processed+pending, n)
+        end
+      elsif candidates.length == n
+        if candidates.reduce(:+) == 2020
+          return candidates
+        end
+      end
+
+      processed.push(candidates.pop)
     end
 
-    return nil
+    return []
   end
 end
 
 values = File.read('./report_repair_input').split("\n").map(&:to_i)
 repair = ReportRepair.new(values)
-repair.find_values
-puts repair.found
+puts repair.find_values(2)
