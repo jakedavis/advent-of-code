aa40b8980de7f635aa7845f227bde69ce2fca2da
Jake Davis <jake.davis5989@gmail.com>
2020-12-09 22:44:38 -0800

    [Day 7] Non-working part 2
    WIP.

--- 2020/7_colored_bags.rb
+++ 2020/7_colored_bags.rb
@@ -28,36 +28,54 @@ class ColoredBags
 
   # Entrypoint method - as long as contains_shiny_gold? operates as expected, this will count the
   # total number of bags that have a shiny gold at some point.
-  def find_shiny_golds
+  def instances_of_color(color)
     @ruleset.select do |k, _|
-      contains_shiny_gold?(k)
+      contains_color?(color, k)
     end.length
   end
 
-  # 
-  def contains_shiny_gold?(bag)
+  # Another entrypoint for summing the total number of bags required for a given color.
+  def total_bags_for_color(color)
+    bags_for_color(color)
+  end
+
+  # Checks if the given bag contains the color provided.
+  def contains_color?(color, bag)
     contained = @ruleset[bag]
 
-    # First base check - if "shiny gold" is here, just bail out.
-    if contained.keys.any? {|k| k == 'shiny gold'}
+    # First base check - if the color is here, just bail out.
+    if contained.keys.any? {|k| k == color}
       return true
     end
 
     # Since we're using recursion, if the given bag contains no further entries, we can safely
-    # assume it does not have a shiny gold. If it's not empty, we need to dig deeper into the hash
-    # to see if any of the sub-bags contain shiny gold.
+    # assume it does not have the color. If it's not empty, we need to dig deeper into the hash
+    # to see if any of the sub-bags contain the given color.
     if contained.empty?
       return false
     else
       return contained.keys.any? do |c, _|
-        contains_shiny_gold?(c)
+        contains_color?(color, c)
       end
     end
+  end
 
-    # I'm not sure this is strictly necessary but D E F E N S I V E  P R O G R A M M I N G
-    return false
+  # 
+  def bags_for_color(color, layer=1)
+    bags = @ruleset[color].values.reduce(&:+) || 0
+    subbags = @ruleset[color].keys.map do |b|
+      bags_for_color(b, layer+1)
+    end.reduce(&:+) || 0
+
+    puts "#{"  " * layer}[#{color}] bags=#{bags} subbags=#{subbags}"
+
+    if color == 'shiny gold'
+      require 'pry'; binding.pry
+    end
+
+    return bags + subbags
   end
 end
 
 bags = ColoredBags.new(File.read('./7_colored_bags_input'))
-puts bags.find_shiny_golds
+puts bags.total_bags_for_color('shiny gold')
