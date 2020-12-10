91c9c3e7ed6b14a32d280cf2f2c36b2a2d2d7f25
Jake Davis <jake.davis5989@gmail.com>
2020-12-09 22:40:51 -0800

    [Day 9] Working part 2 solution
    Adds a contiguous_sum method which finds the sum of the lowest and
    highest numbers contained within the contiguous numbers that sum to the
    invalid number from part 1. Whew.
    
    Works by using Array#take_while and managing a sum variable.

--- 2020/9_xmas_attack.rb
+++ 2020/9_xmas_attack.rb
@@ -8,6 +8,8 @@ class XmasAttack
     @hrange = 24
     @stream = stream
     @queue  = stream[@lrange..@hrange]
+    
+    @invalid = 0
   end
 
   # Kind of confusing to follow. Array#index returns the first object that matches the criteria. We
@@ -30,9 +32,34 @@ class XmasAttack
       qidx
     end
 
-    @stream[@hrange]
+    @invalid = @stream[@hrange]
+  end
+
+  # Finds the contiguous sum of numbers that equal the invalid number. It also sums the resulting
+  # min and max from the array of numbers that compose the sum.
+  def contiguous_sum
+    sum, @lrange = 0, 0
+    
+    while @stream[@lrange] < @invalid
+      taken = @stream[@lrange..].take_while do |i|
+        sum += i
+        sum < @invalid
+      end
+      # Ugly, necessary because Ruby won't take the last element in the above loop if it exactly
+      # equals the invalid number. I tried this with <= and it didn't work. I am still somewhat
+      # confused by this.
+      taken << @stream[@lrange+taken.length]
+
+      return taken.min + taken.max if sum == @invalid
+
+      @lrange += 1
+      sum = 0
+    end
+
+    return nil
   end
 end
 
 x = XmasAttack.new(File.read('./9_xmas_attack_input').split("\n").map(&:to_i))
-puts x.attack
+puts "Invalid number is #{x.attack}"
+puts "Min/max sum is #{x.contiguous_sum}"
