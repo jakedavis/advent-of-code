a41f65c288c09c825f3cbf724080dc6d2029242c
Jake Davis <jake.davis5989@gmail.com>
2020-12-16 16:25:03 -0800

    [Day 16] Working part 2 solution
    This adds a @positions class variable and two new methods that will
    essentially calculate the list of possible fields for every index of a
    ticket, as well as determine which field must correspond to which
    position based on process of elimination.
    
    The second method calculates a product of fields that contain
    "departure" based on values in our ticket. This was the part 2 ask.

--- 2020/ruby/16_ticket_reader.rb
+++ 2020/ruby/16_ticket_reader.rb
@@ -11,6 +11,50 @@ class TicketScanner
     @mine    = process_ticket(myticket)
 
     @valid, @invalid = scan(@tickets)
+
+    @positions = determine_positions
+  end
+
+  # Part 2 asks us to multiply the values on our ticket that correspond to the position of any
+  # field that contains "departure" in its name.
+  def departure_product
+    @positions.select do |k,v|
+      v.include?('departure')
+    end.keys.map do |k|
+      @mine[k]
+    end.reduce(&:*)
+  end
+
+  # This will calculate what field is at what position. The way it does this is by process of
+  # elimination; essentially, it looks for the index that has the least number of possible values.
+  # It assumes this list is of length 1 (not great) and then assigns that list of fields to be
+  # "subtracted" by the next index's list of possible fields. This works if there is only one
+  # additional possibility per index.
+  def determine_positions
+    subtract_fields = []
+    process_positions.sort_by {|k, v| v.length}.map do |position, fields|
+      assigned_field = (fields - subtract_fields)[0]
+      subtract_fields = fields
+
+      [position, assigned_field]
+    end.to_h
+  end
+
+  # This method is WILD. First we need to transpose, as this will organize the ticket values by
+  # their index position. This is good. We want to process by position. We then want to select
+  # fields for which EVERY value at the given position is within ANY range.
+  def process_positions
+    @valid.transpose.map.with_index do |t_by_idx, idx|
+      fs = @fields.select do |k, f|
+        t_by_idx.all? do |v|
+          f.any? do |r|
+            r.include?(v)
+          end
+        end
+      end.keys
+
+      [idx, fs]
+    end.to_h
   end
 
   def process_ticket(ticket)
@@ -61,4 +105,4 @@ tickets = tickets.split("\n").drop(1)
 mine    = mine.split("\n").last
 
 t = TicketScanner.new(fields, tickets, myticket: mine)
-puts t.invalid_sum
+puts t.departure_product
