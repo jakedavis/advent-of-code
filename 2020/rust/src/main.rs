a8b088bab34b8400f149fd42cf1479926344a078
Jake Davis <jake.davis5989@gmail.com>
2021-01-30 22:19:40 -0800

    Factor out input fetching code to main.rs

--- 2020/rust/src/main.rs
+++ 2020/rust/src/main.rs
@@ -1,4 +1,8 @@
 mod day1;
+fn get_input(path: &str) -> String {
+    let f = std::fs::read(path).expect("Invalid path");
+    String::from_utf8(f).expect("Invalid UTF-8")
+}
 
 fn main() {
     match day1::run() {
